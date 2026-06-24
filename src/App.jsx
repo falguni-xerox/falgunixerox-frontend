abfc1060ba307d01233502f354d2d498c0d25c60
falguni-xerox <falguni.xerox@gmail.com>
2026-06-24 20:18:08 +0530

    fix: remove BrowserRouter from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import UploadPage from './UploadPage'
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/" element={<div style={{textAlign:'center', padding:'50px'}}><h2>Home Page</h2></div>} />
       </Routes>
-    </BrowserRouter>
   )
 }
 export default App
