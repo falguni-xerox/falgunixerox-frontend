e9b6e9e2f89b1a566b36ada86daaa8f59ec333c8
falguni-xerox <falguni.xerox@gmail.com>
2026-06-24 20:27:59 +0530

    fix: add wildcard to Home route

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
       <Routes>
         <Route path="/upload" element={<UploadPage />} />
-        <Route path="/" element={<div style={{textAlign:'center', padding:'50px'}}><h2>Home Page</h2></div>} />
+        <Route path="/*" element={<div style={{textAlign:'center', padding:'50px'}}><h2>Home Page</h2></div>} />
       </Routes>
   )
 }
